5454ea7c3d8fd3f1e42398f7c305735b9ecd2e8b
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-02-09 19:12:01 -0300

    Create String_Parser.bat

---
+++ String_Parser.bat
@@ -0,0 +1,36 @@
+@echo off
+
+set /a num=0
+set /a letter=0
+set args=%*
+
+if not defined args (echo No Valid Args & goto :eof)
+
+for %%a in (%args%) do (set temp_param=%%a & call :Start)
+
+:End
+echo.
+echo Characters: %letter%
+echo Numbers: %num%
+echo.
+
+if "%letter%" equ "0" (
+    if "%num%" equ "0" (echo Type: Empty String) else (echo Type: Numeric String)
+    ) else (if "%num%" equ "0" (echo Type: Alphabetic String) else (echo Type: Alphanumeric String))
+
+
+goto :eof
+
+:Start
+set current_char=%temp_param:~0,1%
+if "%current_char%" equ "0" (set /a num+=1 & set temp_param=%temp_param:~1% & goto :Start)
+
+if "%current_char%" equ " " goto :eof
+
+set /a current_char_num=%current_char%
+if %current_char_num% equ 0 (set /a letter+=1) else (set /a num+=1)
+
+set temp_param=%temp_param:~1%
+
+if "%temp_param%" equ " " goto :eof
+goto :Start
