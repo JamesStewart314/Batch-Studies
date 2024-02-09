c04d736772f4e6f04988c67e4d8f0c7a26a524e6
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-02-09 00:14:18 -0300

    Create Real_Time_Program_RAM_Monitor.bat

---
+++ Real_Time_Program_RAM_Monitor.bat
@@ -0,0 +1,81 @@
+::
+:: This code performs an analysis of the real-time RAM
+:: consumption of a program open on the computer.
+:: 
+
+@echo off
+chcp 65001 > nul
+color 0A
+
+setlocal enabledelayedexpansion
+
+    :Main
+    title RAM Monitor - %time:~0,-3%
+
+    echo.
+    echo Enter a Full Process Imagename (Ex.: chrome.exe) :
+    set /p "program_imagename=>>> "
+
+    tasklist | find /i "%program_imagename%" > nul
+
+    if %errorlevel% equ 1 (
+        echo.
+        echo There is no Current Process with this Imagename
+        echo Press Enter to Continue...
+        pause > nul
+        cls & goto :Main
+    )
+
+    :Eval_Ram
+    tasklist | find /i "%program_imagename%" 1> "%tmp%\ram_program_data.tmp"
+    set /a total_ram_usage=0
+    for /f "tokens=5 usebackq" %%a in ("%tmp%\ram_program_data.tmp") do (call :sum_func %%a)
+ 
+    goto :Display_RAM
+
+    :sum_func
+    title RAM Monitor - %time:~0,-3%
+
+    echo %1 | find "." > nul
+
+    set value=%1
+    set value=!value:.=!
+
+    if !errorlevel! == 0 (
+        if not "!value!" == ".=" (
+            set /a total_ram_usage+=!value! > nul
+        )
+    )
+    goto :eof
+
+    :Display_RAM
+    set /a number_size=0
+
+    :loop
+    if "!total_ram_usage:~%number_size%, 1!" neq "" (set /a number_size+=1 & goto :loop)
+
+    choice /t 1 /c "YN" /n /m "" /d Y 1> nul & REM sleep of 1 sec
+
+    if !number_size! geq 7 (if !number_size! leq 9 (goto :GB))
+    if !number_size! geq 4 (if !number_size! leq 6 (goto :MB))
+    if !number_size! geq 1 (if !number_size! leq 3 (goto :KB))
+
+    :GB
+    cls & echo.
+    echo Ram Usage of !program_imagename! in Real Time : !total_ram_usage:~0, 1!,!total_ram_usage:~1, 2! Gb
+    echo (Press CTRL ^+ C to End the Program...)
+    goto :Eval_Ram
+
+    :MB
+    cls & echo.
+    echo Ram Usage of !program_imagename! in Real Time : !total_ram_usage:~0, -3!,!total_ram_usage:~-3, -5! Mb
+    echo (Press CTRL ^+ C to End the Program...)
+    goto :Eval_Ram
+
+    :KB
+    cls & echo.
+    echo Ram Usage of !program_imagename! in Real Time : !total_ram_usage! Kb
+    echo (Press CTRL ^+ C to End the Program...)
+    goto :Eval_Ram
+
+endlocal
