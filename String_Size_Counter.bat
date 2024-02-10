20abef56275da934adbaea6a4fa7514b75e8b618
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-02-09 23:06:30 -0300

    Update String_Size_Counter.bat

--- String_Size_Counter.bat
+++ String_Size_Counter.bat
@@ -1,9 +1,9 @@
 ::
-::         This code analyzes a string, determining the number of spaces and total string size
+::                       This code analyzes a string, determining the number of spaces and total string size
 ::
-:: Usage Example: ' cmd /c "C:\Users\MyUser\Downloads\String_Size_Counter.bat" this is a example of string    to perform  the test '
-::                          /^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\
-::                        | Hypothetical Path of this Program on your Computer |
+::   Usage Example: ' cmd /c "C:\Users\MyUser\Downloads\String_Size_Counter.bat" this is a example of string    to perform  the test '
+::                            /^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\
+::                          | Hypothetical Path of this Program on your Computer |
 ::
 
 @echo off
